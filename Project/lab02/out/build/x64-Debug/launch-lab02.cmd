0afefc3e7ef29069e596f1235b7b3ff3e1553aa6
Xarilaos99 <ch.xarris26@gmail.com>
2022-11-11 20:11:36 +0200

    Upload lab02

---
+++ Project/lab02/out/build/x64-Debug/launch-lab02.cmd
@@ -0,0 +1,9 @@
+cd C:/Users/chxar/source/repos/Xarilaos99/OpenGl-Tutorial/Project/lab02/lab02/
+set 
+
+if [%1]==[--debugger] (
+	SHIFT
+)
+
+"C:\Users\chxar\source\repos\Xarilaos99\OpenGl-Tutorial\Project\lab02\out\build\x64-Debug\lab02.exe"  
+pause
