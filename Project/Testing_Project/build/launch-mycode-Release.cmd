59a08e4f53cca52c0f034516f635a33f21d0e200
Xarilaos99 <ch.xarris26@gmail.com>
2022-11-12 17:35:57 +0200

    Creating Solution

---
+++ Project/Testing_Project/build/launch-mycode-Release.cmd
@@ -0,0 +1,9 @@
+cd C:/Programming/Grafika/OpenGl_Tutorial/OpenGl-Tutorial/Xarilaos99/OpenGl-Tutorial/Project/Testing_Project/Mycode/
+set 
+
+if [%1]==[--debugger] (
+	SHIFT
+)
+
+"C:\Programming\Grafika\OpenGl_Tutorial\OpenGl-Tutorial\Xarilaos99\OpenGl-Tutorial\Project\Testing_Project\build\Release\mycode.exe"  
+pause
